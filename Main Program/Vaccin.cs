f234c8183b285466abe8df76e93e57318a12aae5
Shourov <shourov.ahmed@student.ap.be>
2020-03-17 13:53:03 +0100

    Vaccin klasse volledig aangemaakt

---
+++ Main Program/Vaccin.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Main_Program
+{
+    class Vaccin
+    {
+        Random r = new Random();
+
+        //Overloaded constructor
+        public Vaccin(string naam)
+        {
+            Naam = naam;
+        }
+
+        //Properties
+        public string Naam { get; private set; }
+        public int Oplossing { get; set; } = -1;
+
+        //Methoden
+        public int TryKillCode()
+        {
+            if (Oplossing != 1)
+            {
+                return Oplossing;
+            }
+            else
+            {
+              return r.Next(1, 101);
+            }
+        }
+
+        public void ToonInfo()
+        {
+            Console.WriteLine($"Vaccin: {Naam}");
+            Console.WriteLine(Oplossing);
+
+        }
+
+
+    }
+}
